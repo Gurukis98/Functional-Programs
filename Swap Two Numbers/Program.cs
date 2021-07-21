3a1be2321d63bf555b5cbeecbd60e35f219dce4f
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-21 19:38:53 +0530

    [Guru]Add:Swaping Two Numbers

--- Swap Two Numbers/Program.cs
+++ Swap Two Numbers/Program.cs
@@ -7,11 +7,11 @@ namespace Swap_Two_Numbers
       public static void Main(string[] args)
       {
             int a = 5, b = 10;
-            Console.WriteLine("Before Swap a= " + a + " b= " + b);
+            Console.WriteLine("Before Swap a = " + a + " b= " + b);
             a = a * b;   
             b = a / b;       
             a = a / b;     
-            Console.Write("After Swap a= " + a + " b= " + b);
+            Console.Write("After Swap a = " + a + " b= " + b);
       }
    }
 }
